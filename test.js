8f3bf1806ca8e0423cb740a31b173cb4714b2ea5
Tiennk666 <113416254+Tiennk666@users.noreply.github.com>
2022-09-20 09:51:08 +0700

    Upload file js

--- test.js
+++ test.js
@@ -1,28 +1,14 @@
-  var number_a = document.getElementById('number_a');
-        var number_b = document.getElementById('number_b');
-        number_a.addEventListener('change', function () {
-            number_a = number_a.value;
-
-        })
-        number_b.addEventListener('change', function () {
-            number_b = number_b.value;
-
-        })
-
-
-        var btn_kq = document.getElementById('btn_kq');
-btn_kq.addEventListener('click', function () {
-    if (!isNaN(number_a) && !isNaN(number_b)) {
-
-        var c = 0;
-        if (number_a == 0) {
-            document.getElementById('result').innerHTML = 'Phương trình vô nghiệm';
-        }
-        else {
-            c += -number_b / number_a;
-            document.getElementById('result').innerHTML = 'x = ' + c;
-        }
+var btn_kq = document.getElementById("btn_kq");
+btn_kq.addEventListener("click", function () {
+  var number_a = document.getElementById("number_a").value;
+    var number_b = document.getElementById("number_b").value;
+  if (!isNaN(number_a) && !isNaN(number_b)) {
+   
+    if (number_a == 0) {
+      document.getElementById("result").innerHTML = "Phương trình vô nghiệm";
+    } else {
+     
+      document.getElementById("result").innerHTML = "x = " +  -number_b / number_a;
     }
-    else
-        document.getElementById('result').innerHTML = 'x =';
+  } else document.getElementById("result").innerHTML = "x =";
 });
